f6326b1b82d8c9ccc83162a4bb006bf279b7932b
Hao Luo <yvetterowe1116@gmail.com>
2020-08-06 00:17:45 -0400

    Thinking in SwiftUI Chapter 2 exercise: image loading

---
+++ PhotoList/PhotoList/ContentView.swift
@@ -0,0 +1,115 @@
+//
+//  ContentView.swift
+//  PhotoList
+//
+//  Created by Hao Luo on 8/5/20.
+//  Copyright © 2020 Hao Luo. All rights reserved.
+//
+
+import SwiftUI
+
+struct Photo: Codable, Identifiable {
+    let id: String
+    let author: String
+    let width: CGFloat
+    let height: CGFloat
+    let url: String
+    let download_url: String
+}
+
+final class Remote<DataType>: ObservableObject {
+    
+    enum State {
+        case loading
+        case loaded(DataType)
+    }
+    
+    @Published private(set) var state: State = .loading
+    
+    private let url: URL
+    private let transform: (Data) -> DataType?
+    
+    init(urlString: String, transform: @escaping (Data) -> DataType?) {
+        self.url = URL(string: urlString)!
+        self.transform = transform
+    }
+    
+    func load() {
+        URLSession(configuration: .default)
+            .dataTask(with: url) { [weak self] (data, _, error) in
+                DispatchQueue.main.async {
+                    guard let self = self, let data = data else {
+                        return
+                    }
+                    
+                    if let transformedData = self.transform(data) {
+                        self.state = .loaded(transformedData)
+                    }
+                }
+            }
+        .resume()
+    }
+}
+
+struct PhotoListView: View {
+    @ObservedObject private(set) var remote: Remote<[Photo]> = .init(
+        urlString: "https://picsum.photos/v2/list",
+        transform: { try? JSONDecoder().decode([Photo].self, from: $0) }
+    )
+    
+    var body: some View {
+        NavigationView {
+            switch remote.state {
+            case .loading:
+                Text("loading...")
+                    .onAppear { self.remote.load() }
+            case let .loaded(photos):
+                List {
+                    ForEach(photos) { photo in
+                        NavigationLink(destination: PhotoView(url: photo.download_url)) {
+                            Text(photo.author)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct PhotoView: View {
+    @ObservedObject private(set) var remote: Remote<UIImage>
+    
+    init(url: String) {
+        self._remote = .init(
+            initialValue: .init(
+                urlString: url,
+                transform: { UIImage(data: $0) }
+            )
+        )
+    }
+    
+    var body: some View {
+        switch remote.state {
+        case .loading:
+            Text("loading...")
+                .onAppear { self.remote.load() }
+            
+        case let .loaded(image):
+            Image(uiImage: image)
+                .resizable()
+                .aspectRatio(image.size, contentMode: .fit)
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        PhotoListView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
